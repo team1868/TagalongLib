9cab76d01d18730edcde3a4c36cbb04dbfe83baf
Caitlin <cai.caitlin@gmail.com>
2025-01-22 20:02:52 -0800

    trying to fix build issues:

--- src/test/java/tagalong/subsystems/micro/ElevatorUnitTests.java
+++ src/test/java/tagalong/subsystems/micro/ElevatorUnitTests.java
@@ -12,19 +12,20 @@ import tagalong.subsystems.micro.confs.ElevatorConf;
 
 public class ElevatorUnitTests {
   public double kTol = 2e-1; // stub
+  static final double kCANCoderDelayTime = 0.5; // stub
   public ElevatorConf _elevatorConf;
-
+  public Elevator _elevator;
   private final double _heightTolerance = 1.0; // stub
   private final double _velocityTolerance = 1.0; // stub
 
   @BeforeEach
-  void setup(Elevator elevator) {
-    elevator = new Elevator(_elevatorConf);
+  void setup() {
+    _elevator = new Elevator(_elevatorConf);
   }
 
   @AfterEach
-  void shutdown(Elevator elevator) throws Exception {
-    elevator.setBrakeMode(true);
+  void shutdown() throws Exception {
+    _elevator.setBrakeMode(true);
   }
 
   @Test
@@ -35,25 +36,25 @@ public class ElevatorUnitTests {
   @Test
   public void testVelocity(Elevator elevator, double velocity, boolean withFF) {
     elevator.setElevatorVelocity(velocity, withFF);
-    assertEquals(_velocityTolerance, Math.abs(velocity - elevator.getElevatorVelocityMPS()));
+    assertEquals(
+        _velocityTolerance, Math.abs(Math.abs(velocity - elevator.getElevatorVelocityMPS()))
+    );
   }
   @Test
   public void testProfile(Elevator elevator, double height) {
     elevator.setElevatorProfile(height);
-    assertEquals(_heightTolerance, Math.abs(height - elevator.getElevatorHeightM()));
+    assertEquals(_heightTolerance, Math.abs(elevator.getElevatorHeightM()) - height);
   }
   @Test
   public void testProfileComplex(
       Elevator elevator,
-      double goalPosition,
+      double height,
       double goalVelocityMPS,
       double maxVelocityMPS,
       double maxAccelerationMPS2,
       boolean setCurrentState
   ) {
-    elevator.setElevatorProfile(
-        goalPosition, goalVelocityMPS, maxVelocityMPS, maxAccelerationMPS2, true
-    );
-    assertEquals(_heightTolerance, Math.abs(goalPosition - elevator.getElevatorHeightM()));
+    elevator.setElevatorProfile(height, goalVelocityMPS, maxVelocityMPS, maxAccelerationMPS2, true);
+    assertEquals(_heightTolerance, Math.abs(height - elevator.getElevatorHeightM()));
   }
 }
